94b16d388b8ea7fdac8967faca1254593cac8659
theepag <theepag@live.com>
2020-07-06 22:36:19 +0530

    Changed Sign in page

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -26,75 +26,111 @@ class _SignInState extends State<SignIn> {
     return loading
         ? Loading()
         : Scaffold(
-            backgroundColor: Colors.brown[100],
-            appBar: AppBar(
-              backgroundColor: Colors.brown[400],
-              elevation: 0.0,
-              title: Text('Sinn into Vegetus'),
-              actions: <Widget>[
-                FlatButton.icon(
-                    onPressed: () {
-                      widget.toggleView();
-                    },
-                    icon: Icon(Icons.person),
-                    label: Text("Register"))
-              ],
-            ),
+            resizeToAvoidBottomPadding: false,
+            backgroundColor: Colors.white,
+            // appBar: AppBar(
+            //   backgroundColor: Colors.brown[400],
+            //   elevation: 0.0,
+            //   title: Text('Sinn into Vegetus'),
+            //   actions: <Widget>[
+            //     FlatButton.icon(
+            //         onPressed: () {
+            //           widget.toggleView();
+            //         },
+            //         icon: Icon(Icons.person),
+            //         label: Text("Register"))
+            //   ],
+            // ),
             body: Container(
                 padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
                 child: Form(
                   key: _formKey,
                   child: Column(
                     children: <Widget>[
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        decoration:
-                            TextInputDecoration.copyWith(hintText: 'Email'),
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter an email' : null,
-                        onChanged: (val) {
-                          setState(() {
-                            email = val;
-                          });
-                        },
+                      Container(
+                        padding: new EdgeInsets.fromLTRB(0, 90, 0, 0),
+                        child: Image(
+                          image: AssetImage('graphics/logo.png'),
+                          width: 250.0,
+                        ),
                       ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        decoration:
-                            TextInputDecoration.copyWith(hintText: "Password"),
-                        validator: (val) => val.length < 6
-                            ? 'Enter a password 6+ char long'
-                            : null,
-                        obscureText: true,
-                        onChanged: (val) {
-                          setState(() {
-                            password = val;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      RaisedButton(
-                          color: Colors.blue[400],
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () async {
+                      Container(
+                        height: 50.0,
+                        child: TextFormField(
+                          decoration:
+                              TextInputDecoration.copyWith(hintText: 'Email'),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter an email' : null,
+                          onChanged: (val) {
                             setState(() {
-                              loading = true;
+                              email = val;
                             });
-                            if (_formKey.currentState.validate()) {
-                              dynamic result = await _auth
-                                  .signInWithEmailAndPassword(email, password);
-                              if (result == null) {
-                                setState(() {
-                                  error =
-                                      'Could not sign in with those credentials';
-                                  loading = false;
-                                });
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                        height: 50,
+                        child: TextFormField(
+                          decoration: TextInputDecoration.copyWith(
+                              hintText: "Password"),
+                          validator: (val) => val.length < 6
+                              ? 'Enter a password 6+ char long'
+                              : null,
+                          obscureText: true,
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                        width: 500,
+                        height: 45,
+                        child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0)),
+                            color: Colors.green[600],
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              setState(() {
+                                loading = true;
+                              });
+                              if (_formKey.currentState.validate()) {
+                                dynamic result =
+                                    await _auth.signInWithEmailAndPassword(
+                                        email, password);
+                                if (result == null) {
+                                  setState(() {
+                                    error =
+                                        'Could not sign in with those credentials';
+                                    loading = false;
+                                  });
+                                }
                               }
-                            }
-                          }),
+                            }),
+                      ),
+                      Container(
+                        width: 500,
+                        height: 65,
+                        padding: new EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0)),
+                            color: Colors.green[600],
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              widget.toggleView();
+                            }),
+                      ),
                       SizedBox(height: 12.0),
                       Text(
                         error,
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 const TextInputDecoration = InputDecoration(
-    enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 2.0)),
+    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(width: 1.0, color: Colors.grey),
+        borderRadius: BorderRadius.all(Radius.circular(70))),
+    filled: true,
+    fillColor: Colors.white,
     focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.green, width: 2.0)));
+        borderSide: BorderSide(color: Colors.green, width: 2.0),
+        borderRadius: BorderRadius.all(Radius.circular(70))));
