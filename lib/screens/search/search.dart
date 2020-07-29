5fb063e8734f7500ba4a14895654bebf610e3f76
unknown <you@example.com>
2020-07-29 09:36:00 +0530

    search interface

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vegetus/screens/home/home.dart';
 import 'package:vegetus/services/auth.dart';
 
 class Search extends StatefulWidget {
@@ -8,26 +9,52 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   AuthServices _auth = AuthServices();
+
+  Icon cusIcon = Icon(Icons.search);
+  Widget cusSearchBar = Text("Search");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search"),
-        backgroundColor: Colors.green[600],
         actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Colors.white,
-            ),
-            label: Text('logout'),
-            textColor: Colors.white,
-            onPressed: () async {
-              await _auth.SignOut();
-            },
-          )
-        ],
-      ),
-    );
-  }
-}
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if(this.cusIcon.icon == Icons.search){
+                this.cusIcon = Icon(Icons.cancel);
+                this.cusSearchBar = TextField(
+                textInputAction: TextInputAction.go,
+                decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Search for Vegetables..",
+                hintStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 17.0,
+                ),
+                 ),
+                style: TextStyle(
+                color: Colors.white,
+                fontSize: 17.0,
+                   ),
+                    );
+                   }
+                else{
+                  this.cusIcon = Icon(Icons.search);
+                  this.cusSearchBar = Text("Search");
+                     }
+                    }
+                  );
+                },
+                 icon: cusIcon,
+                ),
+                IconButton(onPressed: () {},
+                icon: Icon(Icons.exit_to_app),
+                ),
+              ],
+               title: cusSearchBar,
+               backgroundColor: Colors.green[600],
+              ),
+            );
+          }
+        }
