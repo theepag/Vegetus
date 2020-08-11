d0e25d120cb858d0fa643f554a1109758da2a4be
theepag <theepag@live.com>
2020-08-11 09:19:09 +0530

    successed passing paramters

--- lib/screens/home/UserProductList.dart
+++ lib/screens/home/UserProductList.dart
@@ -4,27 +4,32 @@ import 'package:provider/provider.dart';
 import '../../models/product.dart';
 
 class UserProductList extends StatefulWidget {
+  final String text;
+  UserProductList({Key key, @required this.text}) : super(key: key);
   @override
-  _UserProductListState createState() => _UserProductListState();
+  _UserProductListState createState() => _UserProductListState(text);
 }
 
 class _UserProductListState extends State<UserProductList> {
+  _UserProductListState(String text);
+
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<List<Product>>(context);
     return Scaffold(
         appBar: AppBar(
-          title: Text("productType"),
+          title: Text(widget.text),
         ),
         body: (products != null)
             ? ListView.builder(
                 itemCount: products.length,
                 itemBuilder: (context, index) {
-                  if (products[index].name == "Tomato") {
+                  if (products[index].name == widget.text) {
                     return Card(
                       child: ListTile(
-                        leading: Image.asset('graphics/broccoli.png'),
-                        title: Text(products[index].name),
+                        leading: Image.asset(
+                            'graphics/${widget.text.toLowerCase()}.png'),
+                        title: Text(products[index].name.toString()),
                         subtitle:
                             Text(products[index].price.toString() + " Rs"),
                       ),
